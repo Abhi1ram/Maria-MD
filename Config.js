5fd85cb52948aa0246e07dcda565a9eeba85a0f6
Abhi1ram <166053246+Abhi1ram@users.noreply.github.com>
2024-04-04 23:47:53 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['7602722782']
+global.ownername = "💙𓆩‎✘professor✘𓆪_✰"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
